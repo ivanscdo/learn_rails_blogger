ce5534e9726b511d0c5eb438fc3820f0cb5bc46a
Ivan Saucedo <ivan.scdo@gmail.com>
2019-01-20 13:46:58 -0600

    190120SU1345 completed iteration 2: created comments model, comments controller, comments form(view)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_19_213929) do
+ActiveRecord::Schema.define(version: 2019_01_20_171926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,19 @@ ActiveRecord::Schema.define(version: 2019_01_19_213929) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "author_name"
+    t.text "body"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
   create_table "test", id: false, force: :cascade do |t|
     t.string "username", limit: 10
     t.string "password", limit: 11
   end
 
+  add_foreign_key "comments", "articles"
 end
